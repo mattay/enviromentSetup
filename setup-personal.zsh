0868a3edb0317fab0141ac9cadbc945421d4a14e
Matthew Anderson <mattay@users.noreply.github.com>
2024-06-10 15:07:16 +1000

    Cleaned up un-used

--- setup-personal.zsh
+++ setup-personal.zsh
@@ -1,18 +1,9 @@
 #!/usr/bin/env zsh
 
 source ./src/brew_install_list.sh
-source ./src/log_message.sh
 
-## Alias
-alias strip-empty="egrep -v '^\s*$'"
-
-# Install local fonts using Homebrew
-log_message "Installing local fonts using Homebrew ..."
+# Install fonts using Homebrew
 brew_install_list "lists/casks-fonts.local" "true"
 
-# Install local applications using Homebrew
-log_message "Installing local apps using Homebrew ..."
+# Install applications using Homebrew
 brew_install_list "lists/casks-personal.local" "true"
-
-# Add more files here as needed
-# install_list "lists/another-list.local" "false"
